35a9da91f14b19f48cff207c8bdd79877e3456c5
徐泽宇 <xzy@9elephas.com>
2017-07-10 16:04:22 +0800

    update md5 utils

--- src/main/java/pers/roamer/boracay/util/MD5Util.java
+++ src/main/java/pers/roamer/boracay/util/MD5Util.java
@@ -10,10 +10,9 @@
 package pers.roamer.boracay.util;
 
 
+import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.lang3.StringUtils;
 import pers.roamer.boracay.BoracayException;
-
-import java.math.BigInteger;
-import java.security.MessageDigest;
 
 /**
  * MD5 工具类
@@ -36,24 +35,22 @@ public class MD5Util {
     /**
      * 对字符串md5加密
      *
-     * @param str 需要加密的字符串
+     * @param input 需要加密的字符串
      *
      * @return 加密后的字符串
      *
      * @author 徐泽宇
      * @since 1.0.0  2016年10月21日 下午3:56:44
      */
-    public static String getMD5(String str) throws BoracayException {
+    public static String getMD5(String input) throws BoracayException {
         try {
+            if (StringUtils.isEmpty(input)){
+                throw new BoracayException("can not get MD5  with null String");
+            }
             // 生成一个MD5加密计算摘要
-            MessageDigest md = MessageDigest.getInstance("MD5");
-            // 计算md5函数
-            md.update(str.getBytes());
-            // digest()最后确定返回md5 hash值，返回值为8为字符串。因为md5 hash值是16位的hex值，实际上就是8位的字符
-            // BigInteger函数则将8位的字符串转换成16位hex值，用字符串来表示；得到字符串形式的hash值
-            return new BigInteger(1, md.digest()).toString(16);
+            return DigestUtils.md5Hex(input) ;
         } catch (Exception e) {
-            throw new BoracayException("MD5 error");
+            throw new BoracayException(e.getMessage());
         }
     }
 }
